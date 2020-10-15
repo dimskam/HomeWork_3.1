1d6883fb8dc4e7d3271624329a93825b4b6dec6a
KamenkovDS <dimskam@gmail.com>
2020-10-15 11:48:29 +0300

    Initial Commit

---
+++ HomeWork_3.1/HomeWork_3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork_3_1App.swift
+//  HomeWork_3.1
+//
+//  Created by Dmitriy Kamenkov on 15.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork_3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
